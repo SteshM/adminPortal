8cd826b9e2727fba3badda3cd9e8a16368bd84a0
SteshM <mbithestella0@gmail.com>
2024-09-27 11:05:36 +0300

    Created the superAdmin table

--- Admin/src/main/java/com/example/Admin/model/AdminSuperAdmin.java
+++ Admin/src/main/java/com/example/Admin/model/AdminSuperAdmin.java
@@ -1,4 +1,25 @@
 package com.example.Admin.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminSuperAdmin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+    private String adminName;
+    private String email;
+    private int depotsAssigned;
+    private String profilePic;
+    private boolean cloud;
+    private String createdOn;
+    private String employeeNo;
+    private String contact;
 }
